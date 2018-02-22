99a424c31668a176d69ee25633ed5a5a02ac07de
kseniya <xeniat@crumbit.org>
2018-02-22 16:02:53 +0100

    Foro changed

--- app/src/main/java/com/example/kseniyaturava/mytest/MovieActivity.java
+++ app/src/main/java/com/example/kseniyaturava/mytest/MovieActivity.java
@@ -1,7 +1,10 @@
 package com.example.kseniyaturava.mytest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MovieActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MovieActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
         setTitle("Movie");
+        //News listeners on images
+        Button button_foro = (Button) findViewById(R.id.button_foro);
+        button_foro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), ForoActivity.class));
+            }
+        });
     }
 }

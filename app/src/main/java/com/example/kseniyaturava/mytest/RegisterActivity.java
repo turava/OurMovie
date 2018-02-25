ff385f07dfd2e269ff15ab4ca047954682f23a51
elicoca <34960530+elicoca@users.noreply.github.com>
2018-02-25 19:51:50 +0100

    Login and Register finished

---
+++ app/src/main/java/com/example/kseniyaturava/mytest/RegisterActivity.java
@@ -0,0 +1,27 @@
+package com.example.kseniyaturava.mytest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        setTitle("Register");
+
+        Button registerButton = (Button) findViewById(R.id.register_button);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
